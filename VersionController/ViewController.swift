d37a941db0384ad6ad1a196c87774bb8fc9e12d6
Venky <venky@venkys-macbook-air.local>
2017-11-27 12:55:39 +0530

    new print statement

---
+++ VersionController/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VersionController
+//
+//  Created by Venky on 11/27/17.
+//  Copyright © 2017 Venky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("first change")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
